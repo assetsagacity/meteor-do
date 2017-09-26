5deba06bdb444e94de1706644a85f3c3c7724612
Xavier Priour <xavier.priour@bubblyware.com>
2017-09-26 02:38:05 +0200

    meteor: install and run ok

---
+++ src/app/meteor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# executes the 'meteor' command in the context of the app
+# - if the app is running, we do an exec
+# - if the app is not running, we'll do a run
+
+name=meteor
+running=`docker ps | grep ${name}_1`
+
+if [ -z "$1" ]; then
+  # no args, user wants to launch meteor server, this should be done with docker-compose up, let's do it
+  docker-compose up
+elif [ "$running" ]; then
+  docker-compose exec $name meteor $*
+else
+  docker-compose run $name meteor $*
+fi
